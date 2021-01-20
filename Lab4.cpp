522b6e704e142a5c890f2e43ac314c14c55e60ca
Огурчик <sergeyazarov1006@gmail.com>
2021-01-20 15:59:14 +0200

    Добавьте файлы проекта.

---
+++ Lab4.cpp
@@ -0,0 +1,12 @@
+﻿// Lab4.cpp : Определяет точку входа для приложения.
+//
+
+#include <Windows.h>
+
+
+
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow)
+{
+   
+}
