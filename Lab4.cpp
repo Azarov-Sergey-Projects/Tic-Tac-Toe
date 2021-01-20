15414a5485b7a3bc45a32b1e374393b3b82ccde8
Огурчик <sergeyazarov1006@gmail.com>
2021-01-20 16:01:12 +0200

    Begin

--- Lab4.cpp
+++ Lab4.cpp
@@ -4,9 +4,56 @@
 #include <Windows.h>
 
 
-
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow)
 {
+    TCHAR szClassName[] = TEXT("Мой класс");
+    HWND hMainWnd;
+    MSG msg;
+    WNDCLASSEX wc;
+    wc.cbSize = sizeof(wc);
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc = WndProc;
+    wc.lpszMenuName = NULL;
+    wc.lpszClassName = szClassName;
+    wc.cbWndExtra = NULL;
+    wc.cbClsExtra = NULL;
+    wc.hIcon = LoadIcon(NULL, IDI_WINLOGO);
+    wc.hIconSm = LoadIcon(NULL, IDI_WINLOGO);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = static_cast<HBRUSH>(GetStockObject(WHITE_BRUSH));
+    wc.hInstance = hInst;
+    if (!RegisterClassEx(&wc))
+    {
+        MessageBox(NULL, TEXT("Не получилось зарегистрировать класс!"), TEXT("Ошибка"), MB_OK);
+        return NULL;
+    }
+
+    hMainWnd = CreateWindow(szClassName, TEXT("Лабораторая работа 4"), WS_OVERLAPPEDWINDOW | WS_VSCROLL, CW_USEDEFAULT, NULL, CW_USEDEFAULT, NULL, static_cast<HWND>(NULL), NULL, static_cast<HINSTANCE>(hInst), NULL);
+    if (!hMainWnd)
+    {
+        MessageBox(NULL, TEXT("Не получилось создать окно!"), TEXT("Ошибка"), MB_OK);
+        return NULL;
+    }
+
+    ShowWindow(hMainWnd, nCmdShow);
+    UpdateWindow(hMainWnd);
+
+    while (GetMessage(&msg, NULL, NULL, NULL))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    return msg.wParam;
    
+}
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+
+    }
 }
